9f54168792979339d0f7f8531202ff45f09a7e5e
panjinshuai <2269845200@qq.com>
2021-05-09 14:20:32 +0800

    init project01

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import axios from 'axios' // 引入axios
+
+Vue.prototype.$axios = axios // 添加axios到Vue的原型对象
+
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
